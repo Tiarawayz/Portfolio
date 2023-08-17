cde0e9cb8c69f4e1032f7e34fec0c9b986237878
Tiarawayz <tiarayancey0@gmail.com>
2023-08-17 16:00:06 -0400

    update frontend.js

--- src/components/frontend.js
+++ src/components/frontend.js
@@ -7,7 +7,7 @@ const Frontpage = () => {
       <h1>Hey I'm Tiara Yancey</h1>
       <h2>Full Stack Developer</h2>
       <h4>I just Graduated from Coding dojo with a certificate in Full Stack Development <h4>
-      </h4> I'm looking for an Jr Role and a feet in the tech industry. </h4>
+      </h4> I'm looking for an Jr Role and a foot in the tech industry. </h4>
       <a href='https://github.com/Tiarawayz' target='blank'><img align="center" width="70px" height="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt='Tiarawayz' /></a>
       <a href="https://www.linkedin.com/in/tiarayancey/" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="tiarayancey" height="40" width="70" /></a>
       <a href="https://twitter.com/@yanceywayz" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="@yanceywayz" height="40" width="70" /></a>
