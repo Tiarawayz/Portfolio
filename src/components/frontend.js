b87793a45dc8d82494b6487b9471e84a2b08766c
Tiarawayz <tiarayancey0@gmail.com>
2023-10-26 00:59:59 -0400

    update fronted.js

--- src/components/frontend.js
+++ src/components/frontend.js
@@ -5,7 +5,7 @@ const Frontpage = () => {
   return (
     <div className='top'>
       <h1>Hey I'm Tiara Yancey</h1>
-      <h2>Full Stack Developer </h2>
+      <h2>Full Stack Developer</h2>
       <h4>I just Graduated from Coding dojo with a certificate in Full Stack Development <h4>
       </h4> I'm looking for an Jr Role and a foot in the tech industry. </h4>
       <a href='https://github.com/Tiarawayz' target='blank'><img align="center" width="70px" height="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt='Tiarawayz' /></a>
