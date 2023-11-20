fa15cd0dcf092bae7a7c59c2617317c01adae9cc
Tiarawayz <tiarayancey0@gmail.com>
2023-11-20 11:57:11 -0500

    update frontend.js

--- src/components/frontend.js
+++ src/components/frontend.js
@@ -4,10 +4,9 @@ import './fronted.css';
 const Frontpage = () => {
   return (
     <div className='top'>
-      <h1>Hey I'm Tiara Yancey</h1>
-      <h2>Full Stack Developer</h2>
-      <h4>I just Graduated from Coding dojo with a certificate in Full Stack Development <h4>
-      </h4> I'm looking for an Jr Role and a foot in the tech industry. </h4>
+      <h1>Hey I'm Tiara Yancey | Software Developer</h1>
+      <h4>
+I am a passionate full stack developer with a year of hands-on experience, driven by an unwavering commitment to excellence.<br></br> I thrive on the challenges of coding, consistently surpassing project expectations and deadlines through my dedicated work ethic. <br></br>Beyond the screen, my love for music fuels my creativity, infusing innovative and harmonious solutions into my projects.<br></br> As a hardworking individual, I take pride in bringing people's ideas to life, collaborating seamlessly with diverse teams to transform visions into tangible results.<br></br> Elevate your team with someone who not only codes with expertise but also brings a unique blend of passion, creativity, and dedication to every project<br></br>I'm ready to make a meaningful impact. </h4>
       <a href='https://github.com/Tiarawayz' target='blank'><img align="center" width="70px" height="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt='Tiarawayz' /></a>
       <a href="https://www.linkedin.com/in/tiarayancey/" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="tiarayancey" height="40" width="70" /></a>
       <a href="https://twitter.com/@yanceywayz" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="@yanceywayz" height="40" width="70" /></a>
