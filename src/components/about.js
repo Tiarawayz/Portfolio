68f54341c213334436ac5a70bf0725d7416a80d8
Tiarawayz <tiarayancey0@gmail.com>
2023-11-30 11:21:46 -0500

    update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ const About = () => {
     <div className='more'>
       <div>
       <h1>More About Me</h1>
-      <h4>As a kid my passions were always computers and music. As I am now an adult it's still the same.<br></br> This is where I shine the most and I can showcase how creative and talented I am. I’m always open to learning new things and improving myself.<br></br> And as a software engineer that's where all my quality will come through and help me become a better engineer.<br></br> The willingness to learn new things, try new things, as well as create new ways to do things will come as easily as it did my whole life.<br></br> I always stand outside the box and inspect everything as I work.<br></br> I can work as a team as well as stand on my own if times call for it.</h4>
+      <h4>As a kid my passions were always computers and music. As I am now an adult it's still the same.<br></br> This is where I shine the most and I can showcase how creative and talented I am. I’m always open to learning new things and improving myself.<br></br> And as a software engineer that's where all my quality will come through and help me become a better engineer.<br></br> The willingness to learn new things, try new things, as well as create new ways to do things will come as easily as it did my whole life.<br></br> I always stand outside the box and inspect everything as I work.<br></br> I can work as a team as well as stand on my own if times call for it. </h4>
       </div>
 
       <div>
